bddf03ea3779fe181da97c0917667df4225d44b5
Kyle Dahlin <kydahlin@gmail.com>
2021-06-02 12:24:20 -0400

    cholera update

--- code/cholera/CholeraSIRW_ODE.R
+++ code/cholera/CholeraSIRW_ODE.R
@@ -1,6 +1,7 @@
-# librarires
+# libraries
 library(deSolve)
 library(reshape2)
+library(tidyverse)
 
 # model equations
 chol <- function(t, y, params){
@@ -11,8 +12,8 @@ chol <- function(t, y, params){
     dI2 <- beta_I2*S2*I2 + beta_W2*S2*W2 - (gamma2 + mu2 + delta2)*I2 + n1*I1 - n2*I2
     dR1 <- gamma1*I1 - mu1*R1 + v1*S1 - m1*R1 + m2*R2
     dR2 <- gamma2*I2 - mu2*R2 + v2*S2 + m1*R1 - m2*R2
-    dW1 <- xi1*I1 - v1*W1 - rho1*W1 + rho2*W2
-    dW2 <- xi2*I2 - v2*W2 + rho1*W1 - rho2*W2
+    dW1 <- xi1*I1 - nu1*W1 - rho1*W1
+    dW2 <- xi2*I2 - nu2*W2 + rho1*W1 - rho2*W2
     ret <- c(dS1, dS2, dI1, dI2, dR1, dR2, dW1, dW2)
     return(list(ret))
   })
@@ -20,24 +21,25 @@ chol <- function(t, y, params){
 
 # define parameters
 params <- c(mu1 = 1E-4, mu2 = 1E-4,                # natural birth/death rate
-            beta_I1 = 2.64E-5, beta_I2 = 2.64E-5,  # transmission rate from people
-            beta_W1 = 1.21E-4, beta_W2 = 1.21E-4,  # transmission rate from water
+            beta_I1 = 2.14E-5, beta_I2 = 2.14E-5,  # transmission rate from people
+            beta_W1 = 1.01E-5, beta_W2 = 1.01E-5,  # transmission rate from water
             v1 = 0, v2 = 0,                        # vaccination rate
             m1 = 0.1, m2 = 0.1,                    # movement rate (non-infected)
             n1 = 0, n2 = 0,                        # movement rate (infected)
             gamma1 = 0.25, gamma2 = 0.25,          # recovery rate
             delta1 = 5E-4, delta2 = 5E-4,          # disease induced mortality
             xi1 = 7.56E-3, xi2 = 7.56E-3,          # pathogen survival rate in water
-            rho1 = 0.1, rho2 = 0.2)                # pathogen movement rate in water
+            nu1 = 7.56E-3, nu2 = 7.56E-3,          # pathogen clearance rate in water
+            rho1 = 0.05, rho2 = 0.05)              # pathogen movement rate in water
 
 # define ICs
-IC <- c(S1 = 5000, S2 = 5000,
-        I1 = 0,    I2 = 10, 
-        R1 = 0,    R2 = 0,
-        W1 = 0,    W2 = 25)
+IC <- c(S1 = 10000-300, S2 = 10000,
+        I1 = 300,    I2 = 10, 
+        R1 = 0,      R2 = 0,
+        W1 = 300,    W2 = 10)
 
 # define time (days)
-t <- seq(0,120,0.01)
+t <- seq(0,200,0.01)
 
 # solve ODE
 out <- ode(y = IC, times = t, func = chol, parms = params)
--- code/cholera/Cholera_model.m
+++ code/cholera/Cholera_model.m
@@ -1,56 +1,8 @@
 %%%%%%%%%%% ODE solution for Cholera SIR two-patch model;%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%  May 31st, 2021 %%%%%%%%%%%
 
-
-% Parameter values 
-mu1 = 1E-4; 
-mu2=mu1; 
-beta1I=2.64E-5;
-beta2I=beta1I;
-beta1W= 1.21E-4;
-beta2W=beta1W;
-m1=.1;
-m2=m1;
-delta1=5E-4;
-delta2=delta1;
-n1=0;
-n2=n1;
-gamma1=.25;
-gamma2=.25;
-xi1=7.56E-3;
-xi2=xi1;
-nu1=xi1;
-nu2=xi1;
-rho1=0;
-rho2=.1;
-
-
-%Reproduction number (ODE with constant transmission rates)
-% F=[betAbar*SstA0/NA,0; 0,betBbar*SstB0/NB];
-% V=[muA+gamaA+phiIAB, -phiIBA;-phiIAB, muB+gamaB+phiIBA];
-% M=F*V^(-1);
-% e=eig(M)
-% % R0A=;%%expression is too long
-% % R0B=;  
-% R0=max(e) 
-
-
-
-
 % Timespan in days
-tspan=0:.1:60;
-
-% Initial conditions
-x10=5000;%S1
-x20=0;%I1
-x30=0;%R1
-x40=0;%W1
-x50=5000;%S2
-x60=10;%I2
-x70=0;%R2
-x80=2;%W2
-ICs = [x10, x20, x30, x40, x50, x60, x70, x80];
-
+tspan=0:1:365;
 
 % Get solutionss
 options = odeset('RelTol',10^-8);
@@ -66,8 +18,19 @@ options = odeset('RelTol',10^-8);
 
 
 figure(1)
-plot( t, y, 'linewidth',2)
-legend()
+subplot(2,2,1)
+plot( t, [x1,x5], 'linewidth',2)
+legend('S1','S2')
+subplot(2,2,2)
+plot( t, [x2,x6], 'linewidth',2)
+legend('I1','I2')
+subplot(2,2,3)
+plot( t, [x3,x7], 'linewidth',2)
+legend('R1','R2')
+subplot(2,2,4)
+plot( t, [x4,x8], 'linewidth',2)
+legend('W1','W2')
+
 % figure(1)
 % plot(t, log(x1),'b', 'linewidth',2)
 % legend('IA')
@@ -112,28 +75,28 @@ legend()
 
 function [dX]=cholera(t,y)  %%phebl=patchy_ebola
 % Parameters:
-% mu1, mu2, beta1I, beta2I, beta1W, beta2W, m1, m2, delta1, delta2, n1, n2,
-% gamma1, gamma2, xi1, xi2, nu1, nu2, rho1, rho2
+% Patch 1
 mu1 = 1E-4; 
-mu2=mu1; 
-beta1I=2.64E-5;
-beta2I=beta1I;
-beta1W= 1.21E-4;
-beta2W=beta1W;
-m1=.1;
-m2=m1;
+beta1I=2.64E-6;
+beta1W=1.21E-4;
+m1=.1; % S,R movement
 delta1=5E-4;
-delta2=delta1;
-n1=0;
-n2=n1;
 gamma1=.25;
-gamma2=.25;
+n1=0.001; % I movement
 xi1=7.56E-3;
+nu1=xi1; % decay of pathogen
+rho1=3E-5; % pathogen transport rate
+% Patch 2
+mu2=mu1; 
+beta2I=beta1I;
+beta2W=beta1W;
+m2=m1;
+delta2=delta1;
+gamma2=.25;
+n2=n1;
 xi2=xi1;
-nu1=xi1;
 nu2=xi1;
-rho1=0;
-rho2=.1;
+rho2=3E-6;
 %Variables: y1=S1, y2=I1, y3=R1, y4=W1, y5=S2, y6=I2, y7=R2, y8=W2
 N1 = y(1) + y(2) + y(3);
 N2 = y(5) + y(6) + y(7);
