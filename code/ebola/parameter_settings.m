14fd8964ca0c479c833c9bee4b988f7d4d139b72
mgrogan1 <70183937+mgrogan1@users.noreply.github.com>
2021-06-03 14:45:06 -0400

    Updated parameters after talking with Suzanna

--- code/ebola/parameter_settings.m
+++ code/ebola/parameter_settings.m
@@ -3,18 +3,18 @@
 %% Initial Conditions
 
 % patch 1
-N1 = 6e6;
-S1 = N1 - 100;
-E1 = 0;
-I1 = 100;
+N1 = 6e4;
+S1 = N1 - 1000;
+E1 = 500;
+I1 = 500;
 H1 = 0;
 D1 = 0;
 R1 = 0;
-%ci = 0;
+ci = 0;
 
 % patch 2
-N2 = 0;
-S2 = 0;
+N2 = 6e4;
+S2 = N2;
 E2 = 0;
 I2 = 0;
 H2 = 0;
@@ -30,11 +30,11 @@ y0 = [S1, E1, I1, H1, D1, R1, S2, E2, I2, H2, D2, R2];
 
 alpha1 = 0.1;
 alpha2 = 0.1;
-gammaI1 = 0.1; %originally 0.02 (Burton)
-gammaI2 = 0.1; %originally 0.02 (Burton)
+gammaI1 = 1/15; %originally 0.02 (Burton)
+gammaI2 = 1/15; %originally 0.02 (Burton)
 gammaH1 = 0.028;
 gammaH2 = 0.028;
-phi1 = 0.236;
+phi1 = 0.236; %average from Burton
 phi2 = 0.236;
 deltaI1 = 0.024;
 deltaI2 = 0.024;
@@ -46,30 +46,32 @@ mu1 = 1e-4;
 mu2 = 1e-4;
 %v1 = 0;
 %v2 = 0;
-m1 = 0;%0.005;
-m2 = 0;%0.005;
+m1 = 0.005;
+m2 = 0.005;
 n1 = 0;
 n2 = 0;
 b1 = 1;
 b2 = 1;
 C1 = 1;
 C2 = 1;
-epsilon1 = 0.01;
-epsilon2 = 0.01;
+epsilon1 = 0.1;
+epsilon2 = 0.1;
 
 % calculate betas based on R0
 R0 = 1.7;
-p = 10; % scale betaI for transmission from D to get betaD
+p = 10; % scale betaI for transmission from D to get betaD % from Julie and Lauren's paper
 
 % patch 1
 %betaI1 = 1e-9; % Burton
 betaI1 = R0/(N1*(alpha1/(alpha1+mu1))*(1/(gammaI1+phi1+deltaI1+mu1))+p*N1*(alpha1/(alpha1+mu1))*(deltaI1/(gammaI1+phi1+deltaI1+mu1))*(1/(xi1)))/1;
-%betaD1 = 5e-7; % Burton
+%betaD1 = 5.5e-7; % Burton
 betaD1 = p*betaI1;
 
 % patch 2
-betaI2 = 1e-9;
-betaD2 = 5e-7;
+%betaI2 = 1e-9;
+betaI2 = betaI1;
+%betaD2 = 5e-7;
+betaD2 = betaD1;
 
 
 % Add parameters to structure 'par'
