b0ba91b7c5966625d21f7748fc1a610c92e62d10
Kyle Dahlin <kydahlin@gmail.com>
2021-06-04 12:50:47 -0400

    plot updates

--- code/cholera/CholeraSIRW_ODE.R
+++ code/cholera/CholeraSIRW_ODE.R
@@ -31,7 +31,7 @@ params <- c(mu1 = 0, mu2 = 0,                      # natural birth/death rate 1E
             beta_I1 = 2.14E-5, beta_I2 = 2.14E-5,  # transmission rate from people
             beta_W1 = 1.01E-5, beta_W2 = 1.01E-5,  # transmission rate from water
             v1 = 0, v2 = 0,                        # vaccination rate
-            m1 = 0.1, m2 = 0.1,                    # movement rate (non-infected)
+            m1 = 0.05, m2 = 0.05,                    # movement rate (non-infected)
             n1 = 0, n2 = 0,                        # movement rate (infected)
             gamma1 = 0.25, gamma2 = 0.25,          # recovery rate
             delta1 = 5E-4, delta2 = 5E-4,          # disease induced mortality
@@ -55,7 +55,7 @@ v2_interp <- approxfun(v2, rule = 2)
 IC <- c(S1 = 10000-10, S2 = 10000-10,
         I1 = 100,    I2 = 10, 
         R1 = 0,      R2 = 0,
-        W1 = 300,    W2 = 10)#,
+        W1 = 100,    W2 = 10)#,
        # c1 = 0,      c2 = 0, # added c to count new cases
        #  V1 = 0,      V2 = 0) # added V to count vaccinations
 
--- code/cholera/Cholera_OC.R
+++ code/cholera/Cholera_OC.R
@@ -25,17 +25,17 @@ lambda = matrix(0, nrow = length(t), ncol = 9)
 lambda_init = rep(0,8)
 names(lambda_init) = paste0("lambda",1:8)
 # bounds
-M1 = 0.15
-M2 = 0.15
+M1 = 0.015
+M2 = 0.015
 
 # setup optimal control parameters
 delta = 0.01
 test = -1
 oc_params <- c(b1 = 1, b2 = 1,
-               C1 = 0.5, C2 = 0.5, 
-               epsilon1  = 1000, epsilon2 = 1000)
+               C1 = 0.125, C2 = 0.125, 
+               epsilon1  = 10, epsilon2 = 10)
 
-# define norm(X,1) command from matlab (! over-writes R "norm" function)
+# define norm(X,1) command from matlab (!! over-writes R "norm" function)
 norm <- function(x){sum(abs(x))}
 
 counter <- 1
@@ -83,21 +83,23 @@ while(test < 0 & counter < 50){
 }
 
 # Collect trajectories and controls
-trajectories <- as.data.frame(x)
-trajectories$v1 = v1
-trajectories$v2 = v2
-trajectories <- melt(trajectories, id = c("time"))
-trajectories$compartment = substr(trajectories$variable,1,1)
-trajectories$compartment = factor(trajectories$compartment, levels = c("S", "I", "R", "W", "v"))
-trajectories$patch = substr(trajectories$variable, 2,2)
+control_trajectories <- as.data.frame(x)
+control_trajectories$v1 = v1
+control_trajectories$v2 = v2
+control_trajectories <- melt(control_trajectories, id = c("time"))
+control_trajectories$compartment = substr(control_trajectories$variable,1,1)
+control_trajectories$compartment = factor(control_trajectories$compartment, levels = c("S", "I", "R", "W", "v"))
+control_trajectories$patch = substr(control_trajectories$variable, 2,2)
+
 
 # Plot trajectories and controls
-trajectories %>% ggplot(aes(x = time, y = value, color = patch)) +
-  geom_point(size=1.5)+
-  geom_line()+
+control_plot <- control_trajectories %>% ggplot(aes(x = time, y = value, color = patch)) +
+  geom_line(lwd=1.5)+
+  geom_line(data=out,aes(x = time, y = value, color = patch),linetype="dashed") +
   facet_wrap(vars(compartment), scales = "free") +
   theme_half_open(12) +
   theme(axis.text=element_text(size=16),
         axis.title=element_text(size=14,face="bold"),
         plot.title = element_text(size=18))
 
+control_plot
