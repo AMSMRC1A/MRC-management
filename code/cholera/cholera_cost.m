53f115f220a673059965191f48b5053ce3b74445
cedholm <65034454+cedholm@users.noreply.github.com>
2021-07-06 12:43:29 -0700

    Break apart the objective Functional
    Now returns all parts of the objective functional -- add to see total

--- code/cholera/cholera_cost.m
+++ code/cholera/cholera_cost.m
@@ -8,7 +8,7 @@
 % Author: George H Lytle
 % Date: June 2021
 %-------------------------------------------
-function [J] = cholera_cost(oc_params, params, x, v1, v2, tfinal, tstep)
+function [J1b,J1c,J1e,J2b,J2c,J2e] = cholera_cost(oc_params, params, x, v1, v2, tfinal, tstep)
 %Translate the parameter vectors
 b1 = oc_params(1);          b2 = oc_params(2);
 C1 = oc_params(3);          C2 = oc_params(4);
@@ -24,11 +24,19 @@ S2 = x(:, 5);   I2 = x(:, 6);   W2 = x(:, 8);
 N = tfinal/tstep;
 
 %Compute the objective functional
-integral1 = b1.*(beta_I1*S1.*I1 + beta_W1*S1.*W1) + C1*v1.*S1 + epsilon1*v1.^2;
-integral2 = b2.*(beta_I2*S2.*I2 + beta_W2*S2.*W2) + C2*v2.*S2 + epsilon2*v2.^2;
-J1 = sum(integral1(1:N))*tstep;
-J2 = sum(integral2(1:N))*tstep;
+integral1b = b1.*(beta_I1*S1.*I1 + beta_W1*S1.*W1) ;
+integral1c = C1*v1.*S1;
+integral1e = epsilon1*v1.^2;
+integral2b = b2.*(beta_I2*S2.*I2 + beta_W2*S2.*W2);
+integral2c = C2*v2.*S2;
+integral2e = epsilon2*v2.^2;
+J1b = sum(integral1b(1:N))*tstep;
+J1c = sum(integral1c(1:N))*tstep;
+J1e = sum(integral1e(1:N))*tstep;
+J2b = sum(integral2b(1:N))*tstep;
+J2c = sum(integral2c(1:N))*tstep;
+J2e = sum(integral2e(1:N))*tstep;
 
-J = J1+J2;
+%J = J1+J2;
 end
 
