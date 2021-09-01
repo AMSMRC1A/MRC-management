5ba472925bfd648d88d6d3b498993d9b2ebf9168
eahowerton <46577370+eahowerton@users.noreply.github.com>
2021-09-01 15:48:27 -0400

    test varying control strategies

---
+++ code/cholera/R_code/analysis/vary_control_strategy.R
@@ -0,0 +1,80 @@
+# libraries
+library(deSolve)
+library(reshape2)
+library(tidyverse)
+library(cowplot)
+library(pracma)
+
+# to paralellize
+library(doParallel)
+library(foreach)
+registerDoParallel(4) # update this 4 if you want to use more cores
+
+
+# load files
+source("CholeraSIRW_ODE.R")
+source("Cholera_params.R")
+source("Cholera_OCfunc.R")
+source("Cholera_adjoints.R")
+
+# initial guesses - controls
+guess_v1 = rep(0,length(times))
+guess_v2 = rep(0, length(times))
+
+# setup optimal control parameters
+delta = 0.01
+oc_params <- c(b1 = 1, b2 = 1,
+               C1 = 0.125, C2 = 0.125, 
+               epsilon1  = 10000, epsilon2 = 10000)
+
+# define parameters to sweep across
+test_params <- expand.grid(m1 = c(0,0.05), 
+                           m2 = c(0,0.05),
+                           control_type = c("unique", "equiv", "max", "none"))
+test_params$test_case <- 1:nrow(test_params)
+
+# calculate OC
+start_time <- Sys.time()
+vary_params <- foreach (i=1:nrow(test_params), .packages = c("deSolve","tidyverse", "pracma")) %dopar% { 
+  apply_oc(change_params = test_params[i,],
+           guess_v1 = guess_v1, guess_v2 = guess_v2, 
+           init_x = IC, bounds = bounds,
+           ode_fn = chol, adj_fn = adj,
+           times = times, params = c(params, oc_params), delta = delta, control_type = test_params[i,"control_type"])
+}
+vary_params <- do.call(rbind, vary_params)
+end_time <- Sys.time()
+end_time - start_time
+
+# reformat output
+j_vals <- lapply(1:length(vary_params), function(i){return(data.frame(test_case = i, j = vary_params[[i]][["j"]]))})
+j_vals <- as.data.frame(do.call(rbind, j_vals))
+j_vals <- left_join(test_params,j_vals)
+mult_oc_params <- lapply(1:length(vary_params), function(i){return(data.frame(test_case = i, vary_params[[i]][["ts"]]))})
+mult_oc_params <- as.data.frame(do.call(rbind, mult_oc_params))
+mult_oc_params <- left_join(test_params,mult_oc_params)
+
+
+# change to long for plotting
+mult_oc_params <- melt(mult_oc_params %>% select(-test_case), 
+                       id = c("m1", "m2", "control_type","time"))
+mult_oc_params$scenario = with(mult_oc_params,ifelse(control_type == "unique", paste0(control_type,": ", variable), as.character(control_type)))
+
+p1 = ggplot(data = mult_oc_params %>% filter(control_type %in% c("unique", "equiv"))) + 
+  geom_line(aes(x = time, y = value, linetype = control_type, color = scenario), size = 1) +
+  guides(linetype = FALSE) +
+  facet_grid(rows = vars(m2), cols = vars(m1), labeller = label_both)+
+  labs(y = "optimal vaccination rate") +
+  scale_color_manual(values = c("black", "red", "blue")) +
+  theme_bw()+
+  theme(legend.position = "bottom")
+
+p2 = ggplot(data = j_vals) + 
+  geom_point(aes(x = as.factor(control_type), y = j, shape = paste0("m1: ", m1,", m2: ", m2)), size = 3) + 
+  geom_path(aes(x = as.factor(control_type), y = j, group = paste0("m1: ", m1,", m2: ", m2)), size = 1) +
+  labs(x = "control strategy", y = "cost") +
+  theme_bw()+
+  theme(legend.position = "bottom",
+        legend.title = element_blank())
+plot_grid(p1, p2)
+ggsave("figures/vary_control_strategies.pdf", width = 14, height = 6)
