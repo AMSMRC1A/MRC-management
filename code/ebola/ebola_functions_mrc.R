274ff7e109cbedda299d423ac4d2add14a6877ff
bdhollin <85178780+bdhollin@users.noreply.github.com>
2021-09-27 16:33:19 -0400

    Add files via upload

--- code/ebola/ebola_functions_mrc.R
+++ code/ebola/ebola_functions_mrc.R
@@ -24,14 +24,16 @@ ebola.ode<-function(t,Y,p){
 }
 
 adjoints.ode<-function(t,Y,p,original_inits){
-  state<- sapply(1:length(x_interp),function(i){x_interp[[i]](t)})
-  names(state) <- colnames(solx)[-1]
-  v1<-v1_interp(t)
-  v2<-v2_interp(t)
   
+  state<- sapply(1:length(p$x_interp),function(i){p$x_interp[[i]](t)})
+  names(state)<-c("S1","E1","I1","H1","D1","R1","S2","E2","I2","H2","D2","R2")
   with(as.list(c(Y,p,state)),{
+    
     N1<-S1+E1+I1+H1+R1
     N2<-S2+E2+I2+H2+R2
+    
+    v1<-v1_interp(t)
+    v2<-v2_interp(t)
     
     dlambda1 <- -( b1*(betaI1*I1 + betaD1*D1) + C1*v1 + lambda1*(mu1 -betaI1*I1 - betaD1*D1 - mu1 - v1 - m1)  + lambda2*(betaI1*I1 + betaD1*D1) + lambda6*v1 + lambda7*m1 );
     dlambda2 <- -( C1*v1  + lambda1*mu1 + lambda2*(-mu1 - alpha1 - m1) + lambda3*alpha1 + lambda8*m1 );
@@ -50,4 +52,217 @@ adjoints.ode<-function(t,Y,p,original_inits){
     return(list(c(dlambda1,dlambda2,dlambda3,dlambda4,dlambda5,dlambda6,
                   dlambda7,dlambda8,dlambda9,dlambda10,dlambda11,dlambda12)))
   })
+}
+
+
+##This function replicates what was previously in the main code.  It takes in
+##initial conditions (inits), parameters (params) as they are currently specified
+##in ebola_params_mrc.R, a vector of the maximum vacination rates M=c(M1,M2), and
+##the time period for the optimal control problem (defaults to 2 years if missing)
+##It returns the results in a named list with the simulated result using the 
+##optimal control,including vaccination rates, (SolX) and the final costs 
+##(J11 (Disease in patch 1), J12 (Disease in patch 2), J21 (vaccination in patch 1)
+##, and J22 (vaccination in patch 2)),
+
+ebola.optim<-function(inits,params,M,times=seq(0,730,by=1)){
+  # initial guesses - controls
+  v1 <- data.frame(times = times, v1 = rep(0,length(times)))
+  v1_interp <- approxfun(v1, rule = 2)
+  v1 = v1[,2]
+  v2 = data.frame(times = times, v2 = rep(0,length(times)))
+  v2_interp <- approxfun(v2, rule = 2)
+  v2 = v2[,2]
+  params<-c(params,v1_interp=v1_interp,v2_interp=v2_interp)
+  # adjoints
+  x = matrix(0, nrow = length(times), ncol = 13)
+  lambda = matrix(0, nrow = length(times), ncol = 13)
+  lambda_init = rep(0,12)
+  names(lambda_init) = paste0("lambda",1:12)
+  
+  # define norm(X,1) command from matlab
+  ##------------------------------------
+  norm_oc <- function(x){colSums(abs(x))}
+  
+  delta<-.01
+  counter <- 0
+  test <- -1
+  solx<-ode(y=Y,
+            times=times,
+            func=ebola.ode,
+            parms=params,
+            method = "ode45")
+  x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+  params<-c(params,x_interp=x_interp)
+  while(test < 0 & counter < 200){
+    counter <- counter + 1
+    # set previous control, state, and adjoint 
+    oldv1 <- v1
+    oldv2 <- v2
+    oldx <- solx
+    oldlambda <- lambda
+    
+    # interpolate v
+    params$v1_interp <- approxfun(times, v1, rule = 2)
+    params$v2_interp <- approxfun(times, v2, rule = 2)
+    
+    
+    # solve states
+      solx <- ode(y = inits, times = times, func = ebola.ode, parms = params)
+    
+    
+    # interpolate x (state)
+    params$x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+    
+    
+    # solve adjoint
+    lambda <- ode(y = lambda_init, times = rev(times), func = adjoints.ode, 
+                  parms = params)
+    
+    lambda <- lambda[nrow(lambda):1,]
+    
+    # calculate v1* and v2*
+    temp_v1 <- with(params,(-C1*(solx[,"S1"]+solx[,"I1"])+lambda[,"lambda1"]*solx[,"S1"]-lambda[,"lambda6"]*solx[,"S1"])/(2*epsilon1))
+    temp_v2 <- with(params,(-C2*(solx[,"S2"]+solx[,"I2"])+lambda[,"lambda7"]*solx[,"S2"]-lambda[,"lambda12"]*solx[,"S2"])/(2*epsilon2))
+    
+    v1 <- pmin(M[1], pmax(0, temp_v1))
+    v1 <- 0.5*(v1 + oldv1)
+    v2 <- pmin(M[2], pmax(0, temp_v2))
+    v2 <- 0.5*(v2 + oldv2)
+    
+    # recalculate test
+    if(length(x) != length(oldx)){browser()}
+    
+    test <- min(delta*norm_oc(data.frame(v1,v2))-norm_oc(data.frame(oldv1,oldv2)-data.frame(v1,v2)),
+                delta*norm_oc(solx[,-1])-norm_oc(oldx[,-1]-solx[,-1]),
+                delta*norm_oc(lambda[,-1])-norm_oc(oldlambda[,-1]-lambda[,-1]))
+    print(counter)
+    print(test)
+    
+  }
+  dt<-.1
+  J11=with(c(params,data.frame(solx)),sum((b1*(betaI1*S1 + betaD1*S1*D1) )*dt))
+  J12=with(c(params,data.frame(solx)),sum((b2*(betaI2*S2*I2 + betaD2*S2*D2))*dt))
+  J21=with(c(params,data.frame(solx)),sum(C1*v1*(S1)+epsilon1*v1^2)*dt)
+  J22=with(c(params,data.frame(solx)),sum(C2*v2*S2+epsilon2*v2^2)*dt)
+  solx<-data.frame(solx,v1,v2)
+  returned<-list(solx,J11,J12,J21,J22)
+  names(returned)<-c("SolX","J11","J12","J21","J22")
+  return(returned)
+}
+
+
+##Optimal control for simulation where infection is introduced (I=10) in patch 1 on day
+##1095.  Optimal control problem begins on *day* and calculates through 2 years post 
+##introduction.  Used to examine the cost of delayed or preemptive vaccination programs.
+##Results were not particularly interesting, but saving incase we want to revisit.
+ebola.optim.timing<-function(params,M,day=0){
+  
+  # initial guesses - controls
+  
+  times<-seq(day,1825)
+  inits=c(S1=params$N1,E1=0,I1=0,H1=0,D1=0,R1=0,
+                 S2=params$N2,E2=0,I2=0,H2=0,D2=0,R2=0)
+  
+  v1 <- data.frame(times = times, v1 = rep(0,length(times)))
+  v1_interp <- approxfun(v1, rule = 2)
+  v1 = v1[,2]
+  v2 = data.frame(times = times, v2 = rep(0,length(times)))
+  v2_interp <- approxfun(v2, rule = 2)
+  v2 = v2[,2]
+  params<-c(params,v1_interp=v1_interp,v2_interp=v2_interp)
+  # adjoints
+  x = matrix(0, nrow = length(times), ncol = 13)
+  lambda = matrix(0, nrow = length(times), ncol = 13)
+  lambda_init = rep(0,12)
+  names(lambda_init) = paste0("lambda",1:12)
+  
+  # define norm(X,1) command from matlab
+  ##------------------------------------
+  norm_oc <- function(x){colSums(abs(x))}
+  
+  delta<-.01
+  counter <- 0
+  test <- -1
+  solx<-ode(y=inits,
+            times=seq(1095,1825),
+            func=ebola.ode,
+            parms=params,
+            method = "ode45")%>%
+    data.frame()
+  if(day<1095){
+    solx<-bind_rows(data.frame(time=seq(day,1094),S1=params$N1,E1=0,I1=0,H1=0,D1=0,R1=0,
+                               S2=params$N2,E2=0,I2=0,H2=0,D2=0,R2=0),
+                    solx)
+  }else if(day>1095){
+    solx<-solx%>%filter(time>=day)
+  }
+  x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+  params<-c(params,x_interp=x_interp)
+  while(test < 0 & counter < 200){
+    counter <- counter + 1
+    # set previous control, state, and adjoint 
+    oldv1 <- v1
+    oldv2 <- v2
+    oldx <- solx
+    oldlambda <- lambda
+    
+    # interpolate v
+    params$v1_interp <- approxfun(times, v1, rule = 2)
+    params$v2_interp <- approxfun(times, v2, rule = 2)
+    
+    
+    # solve states
+    if(day<1095){
+      solx_1 <- ode(y = inits, times = seq(day,1095), func = ebola.ode, parms = params)
+      solx_2 <- ode(y = solx_1[dim(solx_1)[1],-1]+c(-10,0,10,0,0,0,0,0,0,0,0,0), times = seq(1095,1825), func = ebola.ode, parms = params)
+      solx<-bind_rows(data.frame(solx_1),data.frame(solx_2[-1,]))
+      solx.forCosts<-solx
+    }else if(day>=365){
+      solx <- ode(y = inits+c(-10,0,10,0,0,0,0,0,0,0,0,0), times = seq(1095,1825), func = ebola.ode, parms = params)
+      solx.forCosts<-solx
+      solx<-solx%>%
+        data.frame()%>%
+        filter(time>=day)
+      }
+    
+    
+    
+    # interpolate x (state)
+    params$x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+    
+    
+    # solve adjoint
+    lambda <- ode(y = lambda_init, times = rev(times), func = adjoints.ode, 
+                  parms = params)
+    
+    lambda <- lambda[nrow(lambda):1,]
+    
+    # calculate v1* and v2*
+    temp_v1 <- with(params,(-C1*(solx[,"S1"]+solx[,"I1"])+lambda[,"lambda1"]*solx[,"S1"]-lambda[,"lambda6"]*solx[,"S1"])/(2*epsilon1))
+    temp_v2 <- with(params,(-C2*(solx[,"S2"]+solx[,"I2"])+lambda[,"lambda7"]*solx[,"S2"]-lambda[,"lambda12"]*solx[,"S2"])/(2*epsilon2))
+    
+    v1 <- pmin(M[1], pmax(0, temp_v1))
+    v1 <- 0.5*(v1 + oldv1)
+    v2 <- pmin(M[2], pmax(0, temp_v2))
+    v2 <- 0.5*(v2 + oldv2)
+    
+    # recalculate test
+    if(length(solx) != length(oldx)){browser()}
+    
+    test <- min(delta*norm_oc(data.frame(v1,v2))-norm_oc(data.frame(oldv1,oldv2)-data.frame(v1,v2)),
+                delta*norm_oc(solx[,-1])-norm_oc(oldx[,-1]-solx[,-1]),
+                delta*norm_oc(lambda[,-1])-norm_oc(oldlambda[,-1]-lambda[,-1]))
+    print(counter)
+    print(test)
+    
+  }
+  dt<-.1
+  J11=with(c(params,data.frame(solx.forCosts)),sum((b1*(betaI1*S1 + betaD1*S1*D1) )*dt))
+  J12=with(c(params,data.frame(solx.forCosts)),sum((b2*(betaI2*S2*I2 + betaD2*S2*D2))*dt))
+  J21=with(c(params,data.frame(solx.forCosts)),sum(C1*v1*(S1)+epsilon1*v1^2)*dt)
+  J22=with(c(params,data.frame(solx.forCosts)),sum(C2*v2*S2+epsilon2*v2^2)*dt)
+  solx<-data.frame(solx,v1,v2)
+  returned<-list(solx,J11,J12,J21,J22)
+  names(returned)<-c("SolX","J11","J12","J21","J22")
+  return(returned)
 }
