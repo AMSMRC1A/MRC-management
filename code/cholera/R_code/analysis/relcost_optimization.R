5c52e7df9a52825d196d8fbb7cf701f7e9693b2f
Kyle Dahlin <kydahlin@gmail.com>
2021-10-21 17:29:28 -0400

    Update relcost_optimization.R

--- code/cholera/R_code/analysis/relcost_optimization.R
+++ code/cholera/R_code/analysis/relcost_optimization.R
@@ -26,17 +26,80 @@ source("Cholera_analysisfunc.R")
 # - parameter set which maximizes relative cost difference
 # - value of the relative cost difference
 
-# Helper function: get.costs----------------------------------------------------
-# Take in a set of parameters and get the costs of using uniform or unique
-# controls
-get.rel.cost <- function(x) {
-  # Expand input to include both control types
-  test_params <- data.frame(
-    m1 = c(x[1], x[1]),
-    m2 = c(x[2], x[2]),
-    control_type = c("unique", "uniform")
-  ) # temporary. need to find a way to pass the variable names to this function
 
+
+# Define baseline parameters----------------------------------------------------
+# Baseline values derived from "Cholera_params.R"
+# These are: params, IC, and times
+
+# Define variable parameters and their ranges-----------------------------------
+# Initial conditions
+# ** Can't change these with the way apply_oc is set up **
+# Total population sizes
+
+
+# Initial no. infected individuals
+
+# Initial amount of pathogen in water
+
+# Transmission parameters
+# betaI's
+
+# betaW's
+
+# Optimal control settings------------------------------------------------------
+# initial guesses - controls
+guess_v1 <- rep(0, length(times))
+guess_v2 <- rep(0, length(times))
+
+# Cost of cases
+tol = 0.01 # tolerance parameter for optimization
+oc_params <- c(b1 = 1, b2 = 1, # cost of cases
+               C1 = 0.125, C2 = 0.125,  # cost of vaccinations
+               epsilon1  = 10000, epsilon2 = 10000) # non-linearity
+
+# Set up parameter tables-------------------------------------------------------
+## ** The following might be circular **
+
+temp_params <- left_join(as_tibble(as.list(params)),
+                         as_tibble(as.list(oc_params)),
+                         by = character()) %>% 
+  # select which parameters you want to vary
+  select(m1, m2, beta_I1, beta_I2, beta_W1, beta_W2, rho1, rho2, b2, C2)
+temp_params$m1 <- 0.025
+temp_params$m2 <- 0.025
+
+variable.parameters <- data.frame(
+  var_name = names(temp_params),
+  # initial values are determined from our original parameterization
+  value = as.double(temp_params[1,]),
+  # bounds are stuff I made up
+  lower_bounds = c(0, 0, temp_params$beta_I1*1E-2, temp_params$beta_I2*1E-2,
+                   temp_params$beta_W1*1E-2, temp_params$beta_W2*1E-2, 0, 0,
+                   0.1, temp_params$C2*1E-2),
+  upper_bounds = c(1, 1, temp_params$beta_I1*1E1, temp_params$beta_I2*1E1,
+                   temp_params$beta_W1*1E1, temp_params$beta_W2*1E1, 0.05, 0.05,
+                   10,temp_params$C2*1E2)
+)
+
+# Turn the variable.parameters data frame into something that can be put into the "apply_oc" function
+num_vars <- dim(variable.parameters)[1]
+test_params <- tibble(!!variable.parameters$var_name[1] := !!variable.parameters$value[1])
+for (i in 2:num_vars) {
+  test_params <- add_column(test_params,
+                            !!variable.parameters$var_name[i] := !!variable.parameters$value[i])
+}
+test_params <- as.data.frame(test_params)
+
+
+# Functions for optimization: --------------------------------------------------
+# get.rel.costs: get the relative cost difference between using uniform and unique controls
+get.rel.cost <- function(test_params, values) {
+  # Get test params
+  # test_params <- helper_df_func(variable.parameters)
+  # test_params[1,] <- values
+  test_params <- expand_grid(test_params, control_type = c("unique", "uniform"))
+  
   # Run optimal control calculations across test_params dataframe
   vary_params <- foreach(
     i = 1:nrow(test_params),
@@ -66,61 +129,56 @@ get.rel.cost <- function(x) {
   reformat_df <- reformat_mult_params_output(vary_params, test_params)
   # compute relative cost
   rel.cost <- reformat_df$j %>%
-    mutate(j_change = 100 * (j - j[control_type == "uniform"]) / j[control_type == "uniform"]) %>%
+    mutate(j_change = (j - j[control_type == "uniform"]) / j[control_type == "uniform"]) %>%
     filter(control_type == "unique") %>%
     select(j_change)
   return(rel.cost$j_change)
 }
 
-# Define baseline parameters----------------------------------------------------
-# Baseline values derived from "Cholera_params.R"
-# These are: params, IC, and times
-
-# Define variable parameters and their ranges-----------------------------------
-# Initial conditions
-# Total population sizes
-
-
-# Initial no. infected individuals
-
-# Initial amount of pathogen in water
-
-# Transmission parameters
-# betaI's
-
-# betaW's
-
-# Optimal control parameters
-# initial guesses - controls
-guess_v1 <- rep(0, length(times))
-guess_v2 <- rep(0, length(times))
-
-# Cost of cases
-tol = 0.01 # tolerance parameter for optimization
-oc_params <- c(b1 = 1, b2 = 1, # cost of cases
-               C1 = 0.125, C2 = 0.125,  # cost of vaccinations
-               epsilon1  = 10000, epsilon2 = 10000) # non-linearity
-
-# Helper function to put variables togeth
-variable.parameters <- data.frame(
-  var_name = c("m1", "m2"),
-  value = c(0.025, 0.025),
-  lower_bounds = c(0, 0),
-  upper_bounds = c(1, 1)
-)
+# Optimization function
+# Same as above, but sets test_params = test_params
+optim_func <- function(x) {get.rel.cost(test_params,x)}
 
 # Optimization problem----------------------------------------------------------
-optim_func <- function(variable.parameters) {
-
-}
-
-out <- optim(
+# Using 'optim' package
+res_optim <- optim(
   par = variable.parameters$value,
-  fn = get.rel.cost,
+  fn = optim_func,
   method = "L-BFGS-B",
   lower = variable.parameters$lower_bounds,
-  upper = variable.parameters$upper_bounds
-) 
+  upper = variable.parameters$upper_bounds,
+  control=list(trace=6)
+)
+res_optim_SANN <- optim( # simulated annealing. doesn't work with constraints
+  par = variable.parameters$value,
+  fn = optim_func,
+  method = "SANN",
+  #lower = variable.parameters$lower_bounds,
+  #upper = variable.parameters$upper_bounds,
+  control=list(trace=6)
+)
+# Didn't really work.
+
+# Using 'nloptr' package
+opts <- list("algorithm"="NLOPT_GN_DIRECT_NOSCAL",
+             "xtol_rel"=1.0e-8)
+res_nloptr <- nloptr(
+  x0 = variable.parameters$value,
+  eval_f = optim_func,
+  lb = variable.parameters$lower_bounds,
+  ub = variable.parameters$upper_bounds,
+  opts = opts
+)
+#
+
+# Using 'optimization' package
+res_optim_sa <- optim_sa(fun = optim_func,
+                         start = variable.parameters$value,
+                         trace = TRUE,
+                         lower = variable.parameters$lower_bounds,
+                         upper = variable.parameters$upper_bounds,
+                         control = list(t0 = 500, nlimit = 50, r = 0.85,
+                                        rf = 3, ac_acc = 0.1, dyn_rf = TRUE))
 
 ## NOTES
-# Only varying m1 and m2, the largest rel. cost diff. is 1%, when m1=1 and m2=0
+# Only varying m1 and m2, the largest rel. cost diff. is 1%, which occurs when m1=1 and m2=0
