7092eebf547e7a82960026f143b691ded4fb1691
bdhollin <85178780+bdhollin@users.noreply.github.com>
2021-11-04 12:17:39 -0400

    Add files via upload

---
+++ ebola_cost_comparison.R
@@ -0,0 +1,206 @@
+require(deSolve)
+require(tidyverse)
+require(reshape2)
+require(ggpubr)
+require(RColorBrewer)
+source("ebola_functions_mrc.R")
+source("ebola_params_mrc.R")
+
+##Look at changes in optimal vaccination rate and costs associated with disease
+##and vaccination as movement rates change
+
+#set movement rates
+m_values<-seq(-5,-3,by=1)
+m_grid<-expand.grid(m1=m_values,m2=m_values)  #Expands 2 (or more) vectors into a grid
+m_grid<-m_grid%>%filter(abs(m1-m2)<2)
+#Set maximum control levels M1,M2
+maxControl<-c(.1,.1)
+
+#Set timeframe
+times=seq(0,365,by=1)
+
+#set intial conditions (initial outbreak) - 10 I in patch 1
+initial=c(S1=parm$N1-10,E1=0,I1=10,H1=0,D1=0,R1=0,S2=parm$N2,E2=0,I2=0,H2=0,D2=0,R2=0)
+
+v1 <- data.frame(times = times, v1 = rep(0,length(times)))
+v1_interp <- approxfun(v1, rule = 2)
+v1 = v1[,2]
+v2 = data.frame(times = times, v2 = rep(0,length(times)))
+v2_interp <- approxfun(v2, rule = 2)
+v2 = v2[,2]
+parm_temp<-c(parm,v1_interp=v1_interp,v2_interp=v2_interp)
+##Simulate out uncontrolled outbreak------------------------
+uncontrolled<-lapply(1:dim(m_grid)[1],function(x){
+  parm_temp$m1<-10^(m_grid[x,"m1"]) #overwrite variables of interest in the temp
+  parm_temp$m2<-10^(m_grid[x,"m2"]) #parameter set
+  temp<-ode(y=initial,
+            times=times,
+            func=ebola.ode,
+            parms=parm_temp,
+            method = "ode45")%>%
+    data.frame()
+  size<-list(SolX=temp,
+             `Patch 1`=with(c(parm,temp),sum(b1*(betaI1*S1*I1 + betaD1*S1*D1)*(times[2]-times[1]))),
+             `Patch 2`=with(c(parm,temp),sum(b2*(betaI2*S2*I2 + betaD2*S2*D2)*(times[2]-times[1]))))
+  print(x)
+  return(size)
+})
+#Extract outbreak size information
+uncontrolled_cost<-lapply(1:dim(m_grid)[1],function(x){
+  temp<-data.frame("Patch 1"=uncontrolled[[x]]$`Patch 1`,
+                   "Patch 2"=uncontrolled[[x]]$`Patch 2`)
+  return(temp)
+})%>%bind_rows()%>%
+  bind_cols(m_grid)%>%
+  mutate(Total=Patch.1+Patch.2)%>%
+  pivot_longer(cols=c("Patch.1","Patch.2","Total"),
+               names_to = "Type",values_to = "Cost")%>%
+  mutate(m1=10^m1,m2=10^m2,control_type="Uncontrolled")
+uncontrolled_cost$Type<-uncontrolled_cost$Type%>%
+  factor(levels=c("Patch.1","Patch.2","Total"),
+         labels=c("Patch 1 Disease Cost","Patch 2 Disease Cost","Total Cost"))
+
+##Solve Optimal Control Problem for unique controls--------------
+parm_temp<-parm
+unique_control<-lapply(1:dim(m_grid)[1],function(x){
+  parm_temp$m1<-10^(m_grid[x,"m1"]) #overwrite variables of interest in the temp
+  parm_temp$m2<-10^(m_grid[x,"m2"]) #parameter set
+  temp<-ebola.optim(inits=initial,M=maxControl,params=parm_temp,
+                    times=times,maxIter = 100,strictConv = F)
+  return(temp)
+})
+
+#Extract optimal Vaccination for unique plans------------------
+unique_control_vacc<-lapply(1:dim(m_grid)[1],function(x){
+  temp<-data.frame(m1=m_grid[x,"m1"],m2=m_grid[x,"m2"],
+                   unique_control[[x]]$SolX)
+  return(temp)
+})%>%bind_rows()%>%
+  select(m1,m2,time,v1,v2)%>%
+  pivot_longer(cols=c("v1","v2"),names_to = "patch",values_to = "vacc_rate")%>%
+  mutate(m1=paste0("10^",m1),m2=paste0("10^",m2),control_type="Unique")
+
+#Renaming parameters so that they are more interpretable
+unique_control_vacc$m1<-unique_control_vacc$m1%>%
+  factor(levels=paste0("10^",seq(-5,1)),ordered = T)
+unique_control_vacc$m2<-unique_control_vacc$m2%>%
+  factor(levels=paste0("10^",seq(-5,1)),ordered = T)
+
+#Extract cost values---------------------
+unique_control_costs<-lapply(1:dim(m_grid)[1],function(x){
+  temp<-data.frame(m1=m_grid[x,"m1"],m2=m_grid[x,"m2"],
+                   J11=unique_control[[x]]$J11,
+                   J12=unique_control[[x]]$J12,
+                   J21=unique_control[[x]]$J21,
+                   J22=unique_control[[x]]$J22)
+  return(temp)
+})%>%bind_rows()%>%
+  mutate(Total=J11+J12+J21+J22,Total_1=J11+J21,Total_2=J12+J22)%>%
+  pivot_longer(cols=c("J11","J12","J21","J22","Total","Total_1","Total_2"),names_to = "Type",values_to = "Cost")%>%
+  mutate(m1=10^m1,m2=10^m2,control_type="Unique")
+
+#Rename costs to be interpretable
+unique_control_costs$Type<-unique_control_costs$Type%>%
+  factor(levels=c("J11","J12","J21","J22","Total_1","Total_2","Total"),
+         labels=c("Patch 1 Disease Cost","Patch 2 Disease Cost",
+                  "Patch 1 Vaccine Cost","Patch 2 Vaccine Cost",
+                  "Patch 1 Total Cost","Patch 2 Total Cost",
+                  "Total Cost"))
+
+##Solve Optimal Control Problem for same controls--------------
+parm_temp<-parm
+same_control<-lapply(1:dim(m_grid)[1],function(x){
+  parm_temp$m1<-10^(m_grid[x,"m1"]) #overwrite variables of interest in the temp
+  parm_temp$m2<-10^(m_grid[x,"m2"]) #parameter set
+  temp<-ebola.optim(inits=initial,M=maxControl,params=parm_temp,
+                    times=times,maxIter = 100,strictConv = F,sameRate = T)
+  return(temp)
+})
+
+
+#Extract optimal Vaccination for same plans------------------
+same_control_vacc<-lapply(1:dim(m_grid)[1],function(x){
+  temp<-data.frame(m1=m_grid[x,"m1"],m2=m_grid[x,"m2"],
+                   same_control[[x]]$SolX)
+  return(temp)
+})%>%bind_rows()%>%
+  select(m1,m2,time,v1,v2)%>%
+  pivot_longer(cols=c("v1","v2"),names_to = "patch",values_to = "vacc_rate")%>%
+  mutate(m1=paste0("10^",m1),m2=paste0("10^",m2),control_type="Same")
+
+#Renaming parameters so that they are more interpretable
+same_control_vacc$m1<-same_control_vacc$m1%>%
+  factor(levels=paste0("10^",seq(-5,1)),ordered = T)
+same_control_vacc$m2<-same_control_vacc$m2%>%
+  factor(levels=paste0("10^",seq(-5,1)),ordered = T)
+
+#Extract cost values---------------------
+same_control_costs<-lapply(1:dim(m_grid)[1],function(x){
+  temp<-data.frame(m1=m_grid[x,"m1"],m2=m_grid[x,"m2"],
+                   J11=same_control[[x]]$J11,
+                   J12=same_control[[x]]$J12,
+                   J21=same_control[[x]]$J21,
+                   J22=same_control[[x]]$J22)
+  return(temp)
+})%>%bind_rows()%>%
+  mutate(Total=J11+J12+J21+J22,Total_1=J11+J21,Total_2=J12+J22)%>%
+  pivot_longer(cols=c("J11","J12","J21","J22","Total","Total_1","Total_2"),names_to = "Type",values_to = "Cost")%>%
+  mutate(m1=10^m1,m2=10^m2,control_type="Same")
+
+#Rename costs to be interpretable
+same_control_costs$Type<-same_control_costs$Type%>%
+  factor(levels=c("J11","J12","J21","J22","Total_1","Total_2","Total"),
+         labels=c("Patch 1 Disease Cost","Patch 2 Disease Cost",
+                  "Patch 1 Vaccine Cost","Patch 2 Vaccine Cost",
+                  "Patch 1 Total Cost","Patch 2 Total Cost",
+                  "Total Cost"))
+
+##Combine Data tables------------------------------
+vacc_rates<-bind_rows(same_control_vacc,unique_control_vacc)
+costs<-bind_rows(uncontrolled_cost,same_control_costs,unique_control_costs)
+costs$control_type<-costs$control_type%>%
+  factor(levels=c("Uncontrolled","Same","Unique"))
+vacc_rates$m1<-vacc_rates$m1%>%
+  factor(levels=c("10^-3","10^-4","10^-5"),ordered = T)
+vacc_rates$m2<-vacc_rates$m2%>%
+  factor(levels=c("10^-5","10^-4","10^-3"),ordered = T)
+costs$m1<-costs$m1%>%
+  factor(levels=c(10^-3,10^-4,10^-5),
+         labels=c("10^-3","10^-4","10^-5"),ordered = T)
+costs$m2<-costs$m2%>%
+  factor(levels=c(10^-5,10^-4,10^-3),
+         labels=c("10^-5","10^-4","10^-3"),ordered = T)
+
+
+#Plot figure of vaccination rates
+rates_plot<-ggplot(vacc_rates,aes(x=time,y=vacc_rate,color=patch,linetype=control_type))+
+  geom_line(size=1.25)+
+  facet_grid(m1~m2,labeller = "label_both")+
+  ylab("Vaccination Rate")+
+  xlab("Day")+
+  theme_bw()
+
+#Plot Cost Figure
+costs_plot<-ggplot(filter(costs,!Type%in%c("Total Cost","Patch 1 Total Cost","Patch 2 Total Cost")),
+       aes(x=control_type,y=Cost,fill=Type))+
+  geom_col()+
+  facet_grid(m1~m2,labeller = "label_both")+
+  ylab("Cost")+
+  xlab("Control Type")+
+  theme_bw()
+
+#Plot Relative Costs
+costs_relative<-costs%>%
+  filter(Type%in%c("Total Cost"))%>%
+  pivot_wider(names_from = control_type,values_from = Cost)%>%
+  mutate(Unique_rel=Unique/Uncontrolled,Same_rel=Same/Uncontrolled,.keep="unused")%>%
+  pivot_longer(cols=c("Unique_rel","Same_rel"),names_to = "Cost_Type",values_to = "Cost")%>%
+  mutate(movement=paste("m1:",m1,",","m2:",m2))
+
+rel_costs_plot<-ggplot(costs_relative,aes(x=movement,y=Cost,color=Cost_Type,fill=Cost_Type))+
+  geom_col(position="dodge")+
+  xlab("Movement")+
+  ylab("Cost Relative to No Control")+
+  theme_bw()
+
+ggarrange(rates_plot,costs_plot,rel_costs_plot,nrow=1)
---
+++ ebola_functions_mrc.R
@@ -0,0 +1,290 @@
+ebola.ode<-function(t,Y,p){
+  with(as.list(c(Y,p)),{
+    N1<-S1+E1+I1+H1+R1
+    N2<-S2+E2+I2+H2+R2
+    
+    v1<-v1_interp(t)
+    v2<-v2_interp(t)
+    
+    dS1 <- mu1*N1 - betaI1*S1*I1 - betaD1*S1*D1 - mu1*S1 - v1*S1 - m1*S1 + m2*S2
+    dE1 <- betaI1*S1*I1 + betaD1*S1*D1 - mu1*E1 - alpha1*E1 - m1*E1 + m2*E2
+    dI1 <- alpha1*E1 - (mu1 + gammaI1 + phi1 + deltaI1)*I1 - n1*I1 + n2*I2
+    dH1 <- phi1*I1 - (gammaH1 + deltaH1 + mu1)*H1
+    dD1 <- deltaI1*I1 - xi1*D1
+    dR1 <- gammaI1*I1 + gammaH1*H1 + v1*S1 - mu1*R1 - m1*R1 + m2*R2
+    
+    dS2 <- mu2*N2 - betaI2*S2*I2 - betaD2*S2*D2 - mu2*S2 - v2*S2 + m1*S1 - m2*S2
+    dE2 <- betaI2*S2*I2 + betaD2*S2*D2 - mu2*E2 - alpha2*E2 + m1*E1 - m2*E2
+    dI2 <- alpha2*E2 - (mu2 + gammaI2 + phi2 + deltaI2)*I2 + n1*I1 - n2*I2
+    dH2 <- phi2*I2 - (gammaH2 + deltaH2 + mu2)*H2
+    dD2 <- deltaI2*I2 - xi2*D2
+    dR2 <- gammaI2*I2 + gammaH2*H2 + v2*S2 - mu2*R2 + m1*R1 - m2*R2
+    return(list(c(dS1,dE1,dI1,dH1,dD1,dR1,dS2,dE2,dI2,dH2,dD2,dR2)))
+  })
+}
+
+adjoints.ode<-function(t,Y,p,original_inits){
+  
+  state<- sapply(1:length(p$x_interp),function(i){p$x_interp[[i]](t)})
+  names(state)<-c("S1","E1","I1","H1","D1","R1","S2","E2","I2","H2","D2","R2")
+  with(as.list(c(Y,p,state)),{
+    
+    N1<-S1+E1+I1+H1+R1
+    N2<-S2+E2+I2+H2+R2
+    
+    v1<-v1_interp(t)
+    v2<-v2_interp(t)
+    
+    dlambda1 <- -( b1*(betaI1*I1 + betaD1*D1) + C1*v1 + lambda1*(mu1 -betaI1*I1 - betaD1*D1 - mu1 - v1 - m1)  + lambda2*(betaI1*I1 + betaD1*D1) + lambda6*v1 + lambda7*m1 );
+    dlambda2 <- -( C1*v1  + lambda1*mu1 + lambda2*(-mu1 - alpha1 - m1) + lambda3*alpha1 + lambda8*m1 );
+    dlambda3 <- -( b1*(betaI1*S1) + lambda1*(mu1 - betaI1*S1) + lambda2*(betaI1*S1) + lambda3*(-(mu1 + gammaI1 + phi1 + deltaI1 + n1))  + lambda4*phi1 + lambda5*deltaI1 + lambda6*gammaI1 + lambda9*(n1) );
+    dlambda4 <- -(lambda1*(mu1) + lambda4*(-(mu1 + gammaH1 + deltaH1)) + lambda6*(gammaH1));
+    dlambda5 <--( b1*(betaD1*S1) - lambda1*betaD1*S1 + lambda2*betaD1*S1 - lambda5*xi1);
+    dlambda6 <- -( lambda1*mu1 + lambda6*(-mu1 - m1) + lambda12*m1);
+    
+    # patch 2
+    dlambda7 <- -( b2*(betaI2*I2 + betaD2*D2) + C2*v2 + lambda7*(mu2 -betaI2*I2 - betaD2*D2 - mu2 - v2 - m2)  + lambda8*(betaI2*I2 + betaD2*D2) + lambda12*v2 + lambda1*m2);
+    dlambda8 <- -( C2*v2  + lambda7*mu2 + lambda8*(-mu2 - alpha2 - m2) + lambda9*alpha2 + lambda2*m2);
+    dlambda9 <- - (b2*betaI2*S2 + lambda7*(mu2 - betaI2*S2) + lambda8*(betaI2*S2) + lambda9*(-(mu2 + gammaI2 + phi2 + deltaI2 + n2)) + lambda10*phi2 + lambda11*deltaI2 + lambda12*gammaI2 + lambda3*n2); 
+    dlambda10 <- -(lambda7*mu2 + lambda10*(-(mu2 + gammaH2 + deltaH2)) + lambda12*gammaH2);
+    dlambda11 <- -( b2*betaD2*S2 - lambda7*betaD2*S2 + lambda8*betaD2*S2 - lambda11*xi2 );
+    dlambda12 <- -( lambda7*mu2 + lambda12*(-mu2 - m2) + lambda6*m2);
+    return(list(c(dlambda1,dlambda2,dlambda3,dlambda4,dlambda5,dlambda6,
+                  dlambda7,dlambda8,dlambda9,dlambda10,dlambda11,dlambda12)))
+  })
+}
+
+
+##This function replicates what was previously in the main code.  It takes in
+##initial conditions (inits), parameters (params) as they are currently specified
+##in ebola_params_mrc.R, a vector of the maximum vacination rates M=c(M1,M2), and
+##the time period for the optimal control problem (defaults to 2 years if missing)
+##It returns the results in a named list with the simulated result using the 
+##optimal control,including vaccination rates, (SolX) and the final costs 
+##(J11 (Disease in patch 1), J12 (Disease in patch 2), J21 (vaccination in patch 1)
+##, and J22 (vaccination in patch 2)).  Now capable of doing both unique and
+##single control!
+
+ebola.optim<-function(inits,params,M,times=seq(0,730,by=1),maxIter=200,strictConv=T,sameRate=F){
+  # initial guesses - controls
+  v1 <- data.frame(times = times, v1 = rep(0,length(times)))
+  v1_interp <- approxfun(v1, rule = 2)
+  v1 = v1[,2]
+  v2 = data.frame(times = times, v2 = rep(0,length(times)))
+  v2_interp <- approxfun(v2, rule = 2)
+  v2 = v2[,2]
+  params<-c(params,v1_interp=v1_interp,v2_interp=v2_interp)
+  # adjoints
+  x = matrix(0, nrow = length(times), ncol = 13)
+  lambda = matrix(0, nrow = length(times), ncol = 13)
+  lambda_init = rep(0,12)
+  names(lambda_init) = paste0("lambda",1:12)
+  
+  # define norm(X,1) command from matlab
+  ##------------------------------------
+  norm_oc <- function(x){colSums(abs(x))}
+  
+  delta<-.01
+  counter <- 0
+  test <- -1
+  solx<-ode(y=Y,
+            times=times,
+            func=ebola.ode,
+            parms=params,
+            method = "ode45")
+  x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+  params<-c(params,x_interp=x_interp)
+  while(test < 0 & counter < maxIter){
+    counter <- counter + 1
+    # set previous control, state, and adjoint 
+    oldv1 <- v1
+    oldv2 <- v2
+    oldx <- solx
+    oldlambda <- lambda
+    
+    # interpolate v
+    params$v1_interp <- approxfun(times, v1, rule = 2)
+    params$v2_interp <- approxfun(times, v2, rule = 2)
+    
+    
+    # solve states
+    solx <- ode(y = inits, times = times, func = ebola.ode, parms = params)
+    
+    
+    # interpolate x (state)
+    params$x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+    
+    
+    # solve adjoint
+    lambda <- ode(y = lambda_init, times = rev(times), func = adjoints.ode, 
+                  parms = params)
+    
+    lambda <- lambda[nrow(lambda):1,]
+    
+    # calculate v1* and v2*
+    if(sameRate==F){
+      temp_v1 <- with(params,(-C1*(solx[,"S1"]+solx[,"I1"])+lambda[,"lambda1"]*solx[,"S1"]-lambda[,"lambda6"]*solx[,"S1"])/(2*epsilon1))
+      temp_v2 <- with(params,(-C2*(solx[,"S2"]+solx[,"I2"])+lambda[,"lambda7"]*solx[,"S2"]-lambda[,"lambda12"]*solx[,"S2"])/(2*epsilon2))
+      
+      v1 <- pmin(M[1], pmax(0, temp_v1))
+      v1 <- 0.5*(v1 + oldv1)
+      v2 <- pmin(M[2], pmax(0, temp_v2))
+      v2 <- 0.5*(v2 + oldv2)
+    }else{
+      temp_v1 <- with(params,(-C1*(solx[,"S1"]+solx[,"I1"])+lambda[,"lambda1"]*solx[,"S1"]-lambda[,"lambda6"]*solx[,"S1"]-
+                                C2*(solx[,"S2"]+solx[,"I2"])+lambda[,"lambda7"]*solx[,"S2"]-lambda[,"lambda12"]*solx[,"S2"])/(2*epsilon1))
+      v1 <- pmin(M[1], pmax(0, temp_v1))
+      v1 <- 0.5*(v1 + oldv1)
+      v2 <- v1
+    }
+    
+    
+    # recalculate test
+    if(length(x) != length(oldx)){browser()}
+    if(strictConv==T){
+      test <- min(delta*norm_oc(data.frame(v1,v2))-norm_oc(data.frame(oldv1,oldv2)-data.frame(v1,v2)),
+                  delta*norm_oc(solx[,-1])-norm_oc(oldx[,-1]-solx[,-1]),
+                  delta*norm_oc(lambda[,-1])-norm_oc(oldlambda[,-1]-lambda[,-1])) 
+    }else{
+      test <- min(delta*norm_oc(data.frame(v1,v2))-norm_oc(data.frame(oldv1,oldv2)-data.frame(v1,v2)))
+    }
+    x
+    print(counter)
+    print(test)
+    solx_final<-solx
+    
+  }
+  dt<-times[2]-times[1]
+  J11=with(c(params,data.frame(solx)),sum((b1*(betaI1*S1 + betaD1*S1*D1) )*dt))
+  J12=with(c(params,data.frame(solx)),sum((b2*(betaI2*S2*I2 + betaD2*S2*D2))*dt))
+  J21=with(c(params,data.frame(solx)),sum(C1*v1*(S1)+epsilon1*v1^2)*dt)
+  J22=with(c(params,data.frame(solx)),sum(C2*v2*S2+epsilon2*v2^2)*dt)
+  vaxProp1=with(c(params,data.frame(solx)),sum(v1*(S1))*dt)
+  vaxProp2=with(c(params,data.frame(solx)),sum(v2*(S2))*dt)
+  solx<-data.frame(solx_final,v1,v2)
+  returned<-list(solx,J11,J12,J21,J22,vaxProp1,vaxProp2)
+  names(returned)<-c("SolX","J11","J12","J21","J22","vaxProp1","vaxProp2")
+  
+  if(counter==maxIter){
+    returned<-list(solx=NA,J11=NA,J12=NA,J21=NA,J22=NA,vaxProp1=NA,vaxProp2=NA)
+    names(returned)<-c("SolX","J11","J12","J21","J22","vaxProp1","vaxProp2")
+  }
+  return(returned)
+}
+
+
+##Optimal control for simulation where infection is introduced (I=10) in patch 1 on day
+##1095.  Optimal control problem begins on *day* and calculates through 2 years post 
+##introduction.  Used to examine the cost of delayed or preemptive vaccination programs.
+##Results were not particularly interesting, but saving incase we want to revisit.
+ebola.optim.timing<-function(params,M,day=0){
+  
+  # initial guesses - controls
+  
+  times<-seq(day,1825)
+  inits=c(S1=params$N1,E1=0,I1=0,H1=0,D1=0,R1=0,
+                 S2=params$N2,E2=0,I2=0,H2=0,D2=0,R2=0)
+  
+  v1 <- data.frame(times = times, v1 = rep(0,length(times)))
+  v1_interp <- approxfun(v1, rule = 2)
+  v1 = v1[,2]
+  v2 = data.frame(times = times, v2 = rep(0,length(times)))
+  v2_interp <- approxfun(v2, rule = 2)
+  v2 = v2[,2]
+  params<-c(params,v1_interp=v1_interp,v2_interp=v2_interp)
+  # adjoints
+  x = matrix(0, nrow = length(times), ncol = 13)
+  lambda = matrix(0, nrow = length(times), ncol = 13)
+  lambda_init = rep(0,12)
+  names(lambda_init) = paste0("lambda",1:12)
+  
+  # define norm(X,1) command from matlab
+  ##------------------------------------
+  norm_oc <- function(x){colSums(abs(x))}
+  
+  delta<-.01
+  counter <- 0
+  test <- -1
+  solx<-ode(y=inits,
+            times=seq(1095,1825),
+            func=ebola.ode,
+            parms=params,
+            method = "ode45")%>%
+    data.frame()
+  if(day<1095){
+    solx<-bind_rows(data.frame(time=seq(day,1094),S1=params$N1,E1=0,I1=0,H1=0,D1=0,R1=0,
+                               S2=params$N2,E2=0,I2=0,H2=0,D2=0,R2=0),
+                    solx)
+  }else if(day>1095){
+    solx<-solx%>%filter(time>=day)
+  }
+  x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+  params<-c(params,x_interp=x_interp)
+  while(test < 0 & counter < 200){
+    counter <- counter + 1
+    # set previous control, state, and adjoint 
+    oldv1 <- v1
+    oldv2 <- v2
+    oldx <- solx
+    oldlambda <- lambda
+    
+    # interpolate v
+    params$v1_interp <- approxfun(times, v1, rule = 2)
+    params$v2_interp <- approxfun(times, v2, rule = 2)
+    
+    
+    # solve states
+    if(day<1095){
+      solx_1 <- ode(y = inits, times = seq(day,1095), func = ebola.ode, parms = params)
+      solx_2 <- ode(y = solx_1[dim(solx_1)[1],-1]+c(-10,0,10,0,0,0,0,0,0,0,0,0), times = seq(1095,1825), func = ebola.ode, parms = params)
+      solx<-bind_rows(data.frame(solx_1),data.frame(solx_2[-1,]))
+      solx.forCosts<-solx
+    }else if(day>=365){
+      solx <- ode(y = inits+c(-10,0,10,0,0,0,0,0,0,0,0,0), times = seq(1095,1825), func = ebola.ode, parms = params)
+      solx.forCosts<-solx
+      solx<-solx%>%
+        data.frame()%>%
+        filter(time>=day)
+      }
+    
+    
+    
+    # interpolate x (state)
+    params$x_interp <- lapply(2:ncol(solx), function(x){approxfun(solx[,c(1,x)], rule = 2)})
+    
+    
+    # solve adjoint
+    lambda <- ode(y = lambda_init, times = rev(times), func = adjoints.ode, 
+                  parms = params)
+    
+    lambda <- lambda[nrow(lambda):1,]
+    
+    # calculate v1* and v2*
+    temp_v1 <- with(params,(-C1*(solx[,"S1"]+solx[,"I1"])+lambda[,"lambda1"]*solx[,"S1"]-lambda[,"lambda6"]*solx[,"S1"])/(2*epsilon1))
+    temp_v2 <- with(params,(-C2*(solx[,"S2"]+solx[,"I2"])+lambda[,"lambda7"]*solx[,"S2"]-lambda[,"lambda12"]*solx[,"S2"])/(2*epsilon2))
+    
+    v1 <- pmin(M[1], pmax(0, temp_v1))
+    v1 <- 0.5*(v1 + oldv1)
+    v2 <- pmin(M[2], pmax(0, temp_v2))
+    v2 <- 0.5*(v2 + oldv2)
+    
+    # recalculate test
+    if(length(solx) != length(oldx)){browser()}
+    
+    test <- min(delta*norm_oc(data.frame(v1,v2))-norm_oc(data.frame(oldv1,oldv2)-data.frame(v1,v2)),
+                delta*norm_oc(solx[,-1])-norm_oc(oldx[,-1]-solx[,-1]),
+                delta*norm_oc(lambda[,-1])-norm_oc(oldlambda[,-1]-lambda[,-1]))
+    print(counter)
+    print(test)
+    
+  }
+  dt<-.1
+  J11=with(c(params,data.frame(solx.forCosts)),sum((b1*(betaI1*S1*I1 + betaD1*S1*D1) )*dt))
+  J12=with(c(params,data.frame(solx.forCosts)),sum((b2*(betaI2*S2*I2 + betaD2*S2*D2))*dt))
+  J21=with(c(params,data.frame(solx.forCosts)),sum(C1*v1*(S1)+epsilon1*v1^2)*dt)
+  J22=with(c(params,data.frame(solx.forCosts)),sum(C2*v2*S2+epsilon2*v2^2)*dt)
+  solx<-data.frame(solx,v1,v2)
+  returned<-list(solx,J11,J12,J21,J22)
+  names(returned)<-c("SolX","J11","J12","J21","J22")
+  return(returned)
+}
