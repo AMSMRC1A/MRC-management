a817ea89c5c08c0d0f82e7e32d85d2d2deb82acb
eahowerton <46577370+eahowerton@users.noreply.github.com>
2021-12-09 16:48:45 -0500

    add dictionary function

--- code/R/implementation/optimal_control_functions.R
+++ code/R/implementation/optimal_control_functions.R
@@ -1,76 +1,18 @@
 # optimal control --------------------------------------------------------------
 
-#' implement optimal control analysis
+#' implement optimal control optimization
 #' 
-#' @param guess_v1 double of starting value for v1
-#' @param guess_v2 double of starting value for v2
-#' @param guess_u1 double of starting value for u1
-#' @param guess_u2 double of starting value for u2
-#' @param init_x vector initial conditions for states
-#' @param bounds vector of bounds for each control
-#' @param ode_fn function containing ode equations
-#' @param adj_fn function containing adjoint equations
-#' @param times vector of times over which to define optimal solution
-#' @param params vector of model parameters
-#' @param tol tolerance for optimization
+#' @param model string to indicate which model is being run
 #' @param control_type character to define the type of control being implemented;
-#' either \code{"none"} for no control 
-#' or \code{"max"} for control at the upper bound
-#' 
-#' @return list containing \code{trajectories}, a data.frame with all 
-#' time-varying values (state variables, controls, and adjoints), and 
-#' \code{j}, a double of the total cost
-run_oc <- function(guess_v1, guess_v2, 
-                   guess_u1, guess_u2,
-                   init_x, bounds, ode_fn, adj_fn,
-                   times, params, tol = 0.01, control_type) {
-  # initialize variables
-  x <- matrix(0, nrow = length(times), ncol = 9)
-  lambda <- matrix(0, nrow = length(times), ncol = 9)
-  v1 <- guess_v1
-  v2 <- guess_v2
-  u1 <- guess_u1
-  u2 <- guess_u2
-  if (control_type == "uniform") {
-    v2 <- v1
-    u2 <- u1
-  }
-  # final time adjoints
-  lambda_init <- rep(0, 8)
-  names(lambda_init) <- paste0("lambda", 1:8)
-  # implement optimization
-  oc <- oc_optim(
-    v1, v2, u1, u2, x, lambda,
-    IC, lambda_init,
-    bounds, tol, ode_fn, adj_fn,
-    times, params, control_type
-  )
-  return(oc)
-}
-
-#' implement loop for OC optimization
-#' 
-#' used in \code{run_oc()} for the optimization loop
-#' 
-#' @inheritParams run_oc 
-#' @param v1 initial guess for v1
-#' @param v2 initial guess for v2
-#' @param u1 initial guess for u1
-#' @param u2 initial guess for u2
-#' @param x initial guess for x
-#' @param lambda initial guess for lambda
-#' @param lambda_init final time adjoints
+#' either \code{"uniform"} for the same control being applied in both patches 
+#' or \code{"unique"} where control can vary across patches 
 #' 
 #' #' @return list containing \code{trajectories}, a data.frame with all 
 #' time-varying values (state variables, controls, and adjoints), and 
 #' \code{j}, a double of the total cost
-oc_optim <- function(v1, v2, u1, u2, x, lambda, # initial guesses
-                     IC, lambda_init, # state ICs & final time adjoints
-                     bounds, tol, # optimal control settings
-                     ode_fn, adj_fn, # ode and adjoint functions
-                     times, params, control_type # ode and oc parameters
-) {
-  with(as.list(bounds), {
+oc_optim <- function(model,  control_type) {
+  setup <- setup_model(model)
+  with(setup, {
     counter <- 1
     test <- -1
     while (test < 0 & counter < 50) {
@@ -88,7 +30,7 @@ oc_optim <- function(v1, v2, u1, u2, x, lambda, # initial guesses
       u2_interp <- approxfun(times, u2, rule = 2)
       # solve states
       x <- ode(
-        y = IC, times = times, func = ode_fn, parms = params,
+        y = init_x, times = times, func = ode_fn, parms = params,
         v1_interp = v1_interp, v2_interp = v2_interp,
         u1_interp = u1_interp, u2_interp = u2_interp
       )
@@ -104,7 +46,6 @@ oc_optim <- function(v1, v2, u1, u2, x, lambda, # initial guesses
         u1_interp = u1_interp, u2_interp = u2_interp,
         x_interp = x_interp, x = x
       )
-      
       lambda <- lambda[nrow(lambda):1, ]
       lambda_df <- as.data.frame(lambda)
       # calculate v1*, v2*, u1*, u2*
@@ -123,7 +64,7 @@ oc_optim <- function(v1, v2, u1, u2, x, lambda, # initial guesses
       # recalculate test
       test <- min(
         tol * norm_oc(c(v1, v2)) - norm_oc(c(oldv1, oldv2) - c(v1, v2)),
-        tol * norm_oc(c(u1, u2)) - norm_oc(c(oldu1, oldu2) - c(u1, u2)), #EH: ASK SUZANNE ABOUT THIS
+        tol * norm_oc(c(u1, u2)) - norm_oc(c(oldu1, oldu2) - c(u1, u2)),
         tol * norm_oc(x[, -1]) - norm_oc(oldx[, -1] - x[, -1]),
         tol * norm_oc(lambda[, -1]) - norm_oc(oldlambda[, -1] - lambda[, -1])
       )
@@ -148,12 +89,14 @@ oc_optim <- function(v1, v2, u1, u2, x, lambda, # initial guesses
 
 #' calculate time series and cost, j, with no optimization
 #' 
-#' @inheritParams run_oc
+#' @inheritParams oc_optim
 #' 
 #' @return list containing \code{trajectories}, a data.frame with all 
 #' time-varying values (state variables, controls, and adjoints), and 
 #' \code{j}, a double of the total cost
-run_no_optim <- function(bounds, init_x, times, ode_fn, params, control_type) {
+run_no_optim <- function(model, control_type) {
+  setup <- setup_model(model)
+  with(setup, {
   if (control_type == "none") {
     params$v1 <- 0
     params$v2 <- 0
@@ -174,6 +117,7 @@ run_no_optim <- function(bounds, init_x, times, ode_fn, params, control_type) {
   trajectories$u2 <- params$u2
   j <- calc_j(times, out, params)
   return(list(trajectories = trajectories, j = j))
+  })
 }
 
 
@@ -183,4 +127,42 @@ run_no_optim <- function(bounds, init_x, times, ode_fn, params, control_type) {
 #' @param x vector
 norm_oc <- function(x) {
   sum(abs(x))
+}
+
+#' input all model-specific variables for optimal control analysis
+#' 
+#' @param model string to indicate which values to indicate which model to input
+#' 
+#' @return list of initial guesses, intial conditions, optimal control and model
+#' settings, and ode and adjoint functions
+setup_model <- function(model){
+  # final time adjoints
+  lambda_init <- rep(0, 8)
+  names(lambda_init) <- paste0("lambda", 1:8)
+  # "dictionary" for cholera setup
+  if(model == "cholera"){
+    source("models/cholera/cholera_baseline_params.R")
+    source("models/cholera/cholera_functions.R")
+    # initial guesses
+    setup <- list(v1 = guess_v1, 
+                  v2 = guess_v2, 
+                  u1 = guess_u1, 
+                  u2 = guess_u2, 
+                  x = matrix(0, nrow = length(times), ncol = 9),
+                  lambda = matrix(0, nrow = length(times), ncol = 9),
+                  # ICs for ode solver
+                  init_x = IC_cholera, 
+                  lambda_init = lambda_init, 
+                  # optimal control settings
+                  tol = tol_cholera,
+                  bounds = bounds_cholera, 
+                  # functions
+                  ode_fn = ode_cholera, 
+                  adj_fn = adjoint_cholera, 
+                  # model settings
+                  times = times_cholera, 
+                  params = params_cholera, 
+    )
+  }
+  return(setup)
 }
