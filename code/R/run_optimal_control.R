ab3801c9dec72dafea4a6a7642e2ce2ba3c92697
Kyle Dahlin <kydahlin@gmail.com>
2021-12-13 15:57:36 -0500

    Update run_optimal_control.R
    Everything works except the 3rd test with the "setDT" function. See file for more detailed comments

--- code/R/run_optimal_control.R
+++ code/R/run_optimal_control.R
@@ -3,11 +3,13 @@
 library(deSolve)
 library(tidyverse)
 library(pracma)
+#library(reshape2)
+library(data.table)
 # libraries for parallelization
 library(doParallel)
 library(foreach)
-# update this if you want to be less aggressive with your core use
-registerDoParallel(detectCores() - 2) 
+# use all of your CPU cores except 2 for processing 
+registerDoParallel(detectCores() - 2) # decrease for less aggressive core use
 
 source("implementation/optimal_control_functions.R")
 
@@ -38,33 +40,48 @@ test$trajectories %>%
 
 # implement across multiple optimal control parameters -------------------------
 # define parameters to change
-test_params <- data.frame(
-  control_type = c("unique", "uniform")
+test_params <- expand.grid(
+  control_type = c("unique", "uniform"),
+  beta_I1 = c(2.64E-6,2.64E-5)
 )
 
 # iterate over multiple
 # EH Note: is there a more efficient way to do this?
-test <- foreach(
+# KD: maybe. You could create a full parameter set data table, then use mutate
+#     to add the appropriate columns from oc_optim. But I think mutate doesn't
+#     play well with multi-output functions. Maybe one of the apply functions?
+#     Brandon might know more
+
+
+test2 <- foreach(
   i = 1:nrow(test_params),
-  .packages = c("deSolve", "tidyverse", "pracma"),
-  # explicitly give 'foreach' the functions and data it needs
-  .export = c(
-    # optimal control functions
-    "oc_optim", "run_no_optim","norm_oc",
-    "setup_model", "set_old_variables","define_interp_fns", 
-    "update_optimal_solution", "param_changer")
-              # EH: KYLE CAN YOU MAKE SURE WE DON'T NEED THESE NOW?
-              # "adj","calc_opt_v", "calc_opt_u",
-              # "calc_j","eval_j_integrand",
-              # "chol","params", "oc_params")
+  .packages = c("deSolve", "tidyverse", "pracma")
 ) %dopar% {
   oc_optim(
     model = "cholera",
     change_params = test_params[i, ]
   )
 }
+test_params$test_case <- 1:nrow(test_params)
+
+# KD: the following makes the output from the above "nice"
+#     could/should this be built into the oc_optim function?
+
+states <- lapply(1:nrow(test_params), function(i) {
+  return(data.frame(test_case = i, test2[[i]]$trajectories))
+})
+states <- as.data.frame(do.call(rbind, states))
+states <- left_join(test_params, states)
+j_vals <- lapply(1:nrow(test_params), function(i) {
+  return(data.frame(test_case = i, test2[[i]][["j"]]))
+})
+j_vals <- as.data.frame(do.call(rbind, j_vals))
+j_vals <- left_join(test_params, j_vals)
+j_vals$j <- apply(j_vals[, c("j_case1", "j_case2", "j_vacc1", "j_vacc2")], 1, sum)
 
 
-test <- setDT(change_params)[, oc_optim(model = "cholera", 
+# KD: This isn't working for me. I had to add the "data.table" library to get 
+#     the setDT function. "Error: Cannot find symbol change_params"
+test3 <- setDT(change_params)[, oc_optim(model = "cholera", 
                                         change_params = .SD), by = .I]
 
